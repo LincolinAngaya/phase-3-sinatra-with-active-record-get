1de426f9d99975a5d9847185897c4af90312d8a8
Lincoli Angaya <lincolinangaya5626@gmail.com>
2022-09-04 13:36:08 +0300

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < Sinatra::Base
 
+  set :default_content_type, 'application/json'
+
   get '/games' do
     # get all the games from the database
     games = Game.all.order(:title).limit(10)
@@ -9,17 +11,15 @@ class ApplicationController < Sinatra::Base
 
   # use the :id syntax to create a dynamic route
   get '/games/:id' do
-     # look up the game in the database using its ID
-     game = Game.find(params[:id])
-     # send a JSON-formatted response of the game data
-     # game.to_json(include: :reviews)
-     # game.to_json(include: { reviews: { include: :user } })
-       # include associated reviews in the JSON response
-    game.to_json(only: [:id, :title, :genre, :price], include: {
-      reviews: { only: [:comment, :score], include: {
-        user: { only: [:name] }
-      } }
-    })
+      game = Game.find(params[:id])
+      # include associated reviews in the JSON response
+      # game.to_json(include: :reviews)
+      # game.to_json(include: { reviews: { include: :user } })
+      game.to_json(only: [:id, :title, :genre, :price], include: {
+        reviews: { only: [:comment, :score], include: {
+          user: { only: [:name] }
+        } }
+      })
   end
 
 end
